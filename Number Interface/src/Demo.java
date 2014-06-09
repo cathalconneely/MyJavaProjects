801b79060955df0c8f182a8665b30406fe7f03d1
cathalconneely <cathal.conneely1@gmail.com>
2014-06-09 15:26:44 +0100

    Update Demo.java

--- Number Interface/src/Demo.java
+++ Number Interface/src/Demo.java
@@ -10,22 +10,22 @@ public class Demo {
 		System.out.print("original list contents: \t\t");
 		System.out.println(n1.asString()); //Call asString method to show contents of ArrayList
         
-        /* Demonstrate method neg()*/
-        System.out.print("output following neg() method call: ");
-        n1.neg();// call neg() method on n1 object 
-        System.out.println(n1.asString());// Display new contents of ArrayList
-        System.out.println();//blank line
+                /* Demonstrate method neg()*/
+                System.out.print("output following neg() method call: ");
+                n1.neg();// call neg() method on n1 object 
+                System.out.println(n1.asString());// Display new contents of ArrayList
+                System.out.println();//blank line
         
-        /* Demonstrate method sqrt() */
-        Float [] f = new Float[] {55.5f, 66.6f, 77.7f};
+                /* Demonstrate method sqrt() */
+                Float [] f = new Float[] {55.5f, 66.6f, 77.7f};
         
-        NumList n2 = new NumList(f); // Create NumList object with array f as argument
-        System.out.print("original list contents: \t     ");
-        System.out.println(n2.asString()); //show original ArrayList contents
+                NumList n2 = new NumList(f); // Create NumList object with array f as argument
+                System.out.print("original list contents: \t     ");
+                System.out.println(n2.asString()); //show original ArrayList contents
        
-        System.out.print("output following sqrt() method call: ");
-        n2.sqrt();// call sqrt() method on n2 object
-        System.out.println(n2.asString()); // Display new contents
+                System.out.print("output following sqrt() method call: ");
+                n2.sqrt();// call sqrt() method on n2 object
+                System.out.println(n2.asString()); // Display new contents
        
 	}
 
