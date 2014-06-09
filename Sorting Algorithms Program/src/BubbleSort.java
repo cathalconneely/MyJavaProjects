b689dd0fbd7422a6691f3c481ddf62f235bf1c4c
cathalconneely <cathal.conneely1@gmail.com>
2014-06-09 15:12:11 +0100

    Update BubbleSort.java

--- Sorting Algorithms Program/src/BubbleSort.java
+++ Sorting Algorithms Program/src/BubbleSort.java
@@ -11,19 +11,19 @@ public class BubbleSort {
 	}
 	
 	// bubbleSort method to be called in main method on array.
-    public static void bubbleSort(int [] a) { 
+        public static void bubbleSort(int [] a) { 
 	   
-    	int outer, inner; // Declare variables.
+    	    int outer, inner; // Declare variables.
        
-    	for (outer = a.length - 1; outer > 0; outer--) { // counting down 
-		   for (inner = 0; inner < outer; inner++) { // bubbling up 
-			    if (a[inner] > a[inner + 1]) { // if out of order... 
-			    int temp = a[inner]; // ...then swap 
-			    a[inner] = a[inner + 1]; 
-			    a[inner + 1] = temp; 
-			    }
-	       }
-	   }
+    	    for (outer = a.length - 1; outer > 0; outer--) { // counting down 
+		       for (inner = 0; inner < outer; inner++) { // bubbling up 
+			        if (a[inner] > a[inner + 1]) { // if out of order... 
+			        int temp = a[inner]; // ...then swap 
+			        a[inner] = a[inner + 1]; 
+			        a[inner + 1] = temp; 
+			        }
+	               }
+	    }
 
 	} // End bubbleSort method.      
 
