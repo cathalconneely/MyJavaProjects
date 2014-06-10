d7fed9c32d66452438a21352f5810744bc65653c
cathalconneely <cathal.conneely1@gmail.com>
2014-06-10 15:45:38 +0100

    Update CreateThread.java

--- Token Categorizer GUI/src/guiProject/CreateThread.java
+++ Token Categorizer GUI/src/guiProject/CreateThread.java
@@ -19,56 +19,60 @@ public class CreateThread extends Thread{ // extends thread class to allow multi
 	  //constrcutor
 	  public CreateThread(String f, String l, String c) {
 		  
-		  myFile=f;
+              myFile=f;
 	      myLanguage=l;
 	      myCountry=c;
 	  }
 
 
-   // override run() method with method to read in file and categorize tokens
-   public void run(){
+          // override run() method with method to read in file and categorize tokens
+          public void run(){
 
-        Locale myLocale = new Locale(myLanguage, myCountry); // create locale
-        // Create three lists -  one to contain strings, one to contain integers, and one to contain real numbers
-        ArrayList<String> stringList = new ArrayList<String>();
-        ArrayList<Integer> intList = new ArrayList<Integer>();
-        ArrayList<Double> doubleList = new ArrayList<Double>();
+               Locale myLocale = new Locale(myLanguage, myCountry); // create locale
+               // Create three lists -  one to contain strings, one to contain integers, and one to contain real numbers
+               ArrayList<String> stringList = new ArrayList<String>();
+               ArrayList<Integer> intList = new ArrayList<Integer>();
+               ArrayList<Double> doubleList = new ArrayList<Double>();
 
-        Scanner fileIn = null; // Create Scanner
+               Scanner fileIn = null; // Create Scanner
 
-        try {
-        	// Use scanner to read in file
-            fileIn = new Scanner(new BufferedReader(new FileReader(myFile)));
+               try {
+        	      // Use scanner to read in file
+                      fileIn = new Scanner(new BufferedReader(new FileReader(myFile)));
 
-            fileIn.useLocale(myLocale); // use locale on read in contents of file
+                      fileIn.useLocale(myLocale); // use locale on read in contents of file
              
-            // while file has next token
-            while (fileIn.hasNext()) {
-                 // if scanner has next int
-                if (fileIn.hasNextInt()) {
-                    intList.add(fileIn.nextInt()); // add to int array list
-                } else if (fileIn.hasNextDouble()) { // else if scanner has next double
-                    doubleList.add(fileIn.nextDouble()); // add to double arraylist
-                } else { // else add others to string array list
-                    stringList.add(fileIn.next());
-                }
+                      // while file has next token
+                      while (fileIn.hasNext()) {
+                           
+                           // if scanner has next int
+                          if (fileIn.hasNextInt()) {
+                              intList.add(fileIn.nextInt()); // add to int array list
+                          } else if (fileIn.hasNextDouble()) { // else if scanner has next double
+                              doubleList.add(fileIn.nextDouble()); // add to double arraylist
+                          } else { // else add others to string array list
+                            stringList.add(fileIn.next());
+                        }
 
-            }
+                    }
 
-        } catch (FileNotFoundException e) {
+                 } catch (FileNotFoundException e) {
 
-			e.printStackTrace();
+		        	e.printStackTrace();
 		} finally {
-        	fileIn.close(); // close scanner
-            
-        }
-     // Declare and create new JFrame object
-        CreateFrame jframe = new CreateFrame(myFile, myLanguage, myCountry);
-        /*
-         * call populateTable() method which takes the three arraylists of different types (string, int, double) as parameters
-         */
-        jframe.populateTable(stringList, doubleList, intList);
-       jframe.setVisible(true);
+        	           fileIn.close(); // close scanner
+                  }
+               
+               // Declare and create new JFrame object
+               CreateFrame jframe = new CreateFrame(myFile, myLanguage, myCountry);
+              
+              /*
+               * call populateTable() method which takes the three arraylists of different types (string, int, double) as parameters
+               */
+             
+              jframe.populateTable(stringList, doubleList, intList);
+              jframe.setVisible(true);
       
-    }
-}
+    } // end run()
+
+} // end class CreateThread
